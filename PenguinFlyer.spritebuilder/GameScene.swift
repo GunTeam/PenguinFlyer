f972694035ac0a3de57c7c56585ec069ffa15369
Jorrie Brettin <jorrieb@mit.edu>
2015-05-16 22:34:59 -0400

    Skeleton classes

---
+++ PenguinFlyer.spritebuilder/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  PenguinFlyer
+//
+//  Created by Jottie Brerrin on 5/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class GameScene:CCNode{
+    
+}
---
+++ PenguinFlyer.spritebuilder/Penguin.swift
@@ -0,0 +1,13 @@
+//
+//  Penguin.swift
+//  PenguinFlyer
+//
+//  Created by Jottie Brerrin on 5/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Penguin: CCSprite {
+    //
+}
