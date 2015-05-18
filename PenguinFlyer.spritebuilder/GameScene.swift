f6362a4e3fffe4c437ae70a0256e7e215c2e721f
Jorrie Brettin <jorrieb@mit.edu>
2015-05-17 22:23:15 -0400

    Environment movement skeleton started

--- PenguinFlyer.spritebuilder/GameScene.swift
+++ PenguinFlyer.spritebuilder/GameScene.swift
@@ -13,13 +13,19 @@ class GameScene:CCNode{
     
     var _environment:CCNode!
     var _penguin:Penguin!
+    var _physicsNode:CCPhysicsNode!
     
     func didLoadFromCCB(){
         self.userInteractionEnabled = true
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        NSLog("NS log works!")
         _environment.positionInPoints = CGPointMake(_environment.positionInPoints.x, _environment.positionInPoints.y + 1)
+    }
+    
+    func TogglePenguinSpeed(){
+        NSLog("Penguin speed toggled")
     }
     
     override func update(delta: CCTime) {
